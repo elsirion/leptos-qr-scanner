14d990eec646bff26e1086758c35bf2102aff632
elsirion <elsirion@protonmail.com>
2023-08-15 17:45:10 +0200

    chore: move example usage into example dir

--- src/lib.rs
+++ src/lib.rs
@@ -127,39 +127,3 @@ pub fn Scan<A, F>(cx: Scope, active: A, on_scan: F) -> impl IntoView
         </div>
   }
 }
-
-
-#[component]
-pub fn App(cx: Scope) -> impl IntoView {
-    let (scan_signal, scan_set) = create_signal(cx, false);
-    let checkbox_ref = create_node_ref::<Input>(cx);
-
-    view! { cx,
-        <Title text="QR Scanner Demo"/>
-
-        <Scan
-            active=scan_signal
-            on_scan=move |a| {
-                log!("scanned: {}", &a);
-            }
-        />
-        "Scan "
-        <input
-            type="checkbox"
-            ref=checkbox_ref
-            on:change=move |e| {
-                let checked = checkbox_ref.get().expect("<input> to exist").checked();
-                scan_set.set(checked);
-            }
-        />
-  }
-}
-
-pub fn main() {
-    console_error_panic_hook::set_once();
-    mount_to_body(move |cx| {
-        view! { cx,
-            <App />
-        }
-    });
-}
