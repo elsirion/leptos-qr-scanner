e437e322bfd4d7e81e133cc028412115fee90e28
ngutech21 <ngutech21@pm.me>
2024-02-05 09:52:05 +0100

    chore: upgrade to leptos 0.6.5

--- examples/scanner/src/main.rs
+++ examples/scanner/src/main.rs
@@ -1,41 +1,42 @@
+use leptos::html::Input;
+use leptos::logging::log;
 use leptos::*;
 use leptos_qr_scanner::Scan;
-use leptos::html::Input;
 
 #[component]
-pub fn App(cx: Scope) -> impl IntoView {
-    let (scan_signal, scan_set) = create_signal(cx, true);
-    let checkbox_ref = create_node_ref::<Input>(cx);
+pub fn App() -> impl IntoView {
+    let (scan_signal, scan_set) = create_signal(true);
+    let checkbox_ref = create_node_ref::<Input>();
 
-    let (result_signal, set_result) = create_signal(cx, "".to_string());
+    let (result_signal, set_result) = create_signal("".to_string());
 
-    view! { cx,
-        <Scan
-            active=scan_signal
-            on_scan=move |a| {
-                log!("scanned: {}", &a);
-                set_result.set(a);
-            }
-            class=""
-        />
-        "Scan "
-        <input
-            type="checkbox"
-            ref=checkbox_ref
-            checked
-            on:change=move |_e| {
-                let checked = checkbox_ref.get().expect("<input> to exist").checked();
-                scan_set.set(checked);
-            }
-        />
-        <p>"Scan result: " {result_signal} </p>
-  }
+    view! {
+          <Scan
+              active=scan_signal
+              on_scan=move |a| {
+                  log!("scanned: {}", &a);
+                  set_result.set(a);
+              }
+              class=""
+          />
+          "Scan "
+          <input
+              type="checkbox"
+              ref=checkbox_ref
+              checked
+              on:change=move |_e| {
+                  let checked = checkbox_ref.get().expect("<input> to exist").checked();
+                  scan_set.set(checked);
+              }
+          />
+          <p>"Scan result: " {result_signal} </p>
+    }
 }
 
 pub fn main() {
     console_error_panic_hook::set_once();
-    mount_to_body(move |cx| {
-        view! { cx,
+    mount_to_body(move || {
+        view! {
             <App />
         }
     });
